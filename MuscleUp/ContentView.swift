417d8c9b7395f0eb150d0c37451e44255581480b
Bryan Battu <bryan.battu@gmail.com>
2024-04-13 13:13:35 +0200

    Initial Commit

---
+++ MuscleUp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MuscleUp
+//
+//  Created by Bryan Battu on 13/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MuscleUp/MuscleUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuscleUpApp.swift
+//  MuscleUp
+//
+//  Created by Bryan Battu on 13/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct MuscleUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
