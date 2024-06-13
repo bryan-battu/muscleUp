da10ea9312e06b661d514dfdcb3f4360f76d86cf
Bryan Battu <bryan.battu@gmail.com>
2024-06-13 22:38:06 +0200

    UI Session Detail + Realm Serie

---
+++ MuscleUp/View/Session/ExerciceDetail.swift
@@ -0,0 +1,18 @@
+//
+//  ExerciceDetail.swift
+//  MuscleUp
+//
+//  Created by Bryan Battu on 06/06/2024.
+//
+
+import SwiftUI
+
+struct ExerciceDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExerciceDetail()
+}
---
+++ MuscleUp/View/Session/SerieDetail.swift
@@ -0,0 +1,18 @@
+//
+//  SerieDetail.swift
+//  MuscleUp
+//
+//  Created by Bryan Battu on 06/06/2024.
+//
+
+import SwiftUI
+
+struct SerieDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SerieDetail()
+}
